1c7de423a247bee0368f3fe238484e45a67d95b2
allen russ <allenlru55@gmail.com>
2022-12-25 03:09:36 -0700

    refine next joke code, add get next joke when timer runs out

--- code.js
+++ code.js
@@ -3,36 +3,9 @@ let counter = document.getElementById('counter')
 let pauseButton = document.getElementById('hold')
 let interval = timer()
 
-let playing = true;
-function timer(){
-    return startCounter();
-}
-
-
-function startCounter(){
-    return setInterval(function(){
-        let a = document.getElementById('counter'),b = parseInt(a.innerText)
-        if(b>0){a.innerText = b-1
-        }
-    }, 1000)
-}
-
-pauseButton.addEventListener('click', function(){
-    playing?(playing=false,clearInterval(interval),this.innerText="resume"):(playing=true,interval=timer(),this.innerText='hold')
-})
-next.addEventListener('click', function(){
-    deleteCurrentJoke();
-    getJokeFromAPI();
-})
-previous.addEventListener('click', function(){
-    getOldJoke();
-})
-
-
-
-function renderJoke(jokeData){
-    let card = document.createElement('h2');
-    // card.style.textAlign = 'center'
+let card = document.createElement('h2');
+// card.style.textAlign = 'center'
+    card.className = 'card'
     card.setAttribute('style', 'white-space: pre;');
     card.style.font = 'Serif'
     card.style.display = 'block'
@@ -40,8 +13,59 @@ function renderJoke(jokeData){
     card.style.width = '50%'
     card.style.alignSelf = 'center'
     card.style.borderStyle = 'outset'
-    card.className = 'card'
-    let jokes = document.querySelector('#jokes')
+let jokes = document.querySelector('#jokes')
+// let oldJoke = document.createElement('h2');
+//     oldJoke.id = 'oldJoke';
+//     oldJoke.setAttribute('style', 'white-space: pre;');
+//     oldJoke.style.font = 'Serif'
+//     oldJoke.style.margin = 'auto'
+//     oldJoke.style.width = '50%'
+let oldJoke = ""
+let oldJokeArray = [];
+
+
+
+let playing = true;
+function timer(){
+    return startCounter();
+}
+function resetTimer(){
+    let a=document.getElementById('counter'), b=parseInt(a.innerText)
+    b=60;
+    a.innerText = b;
+}
+
+function startCounter(){
+    return setInterval(function(){
+        let a = document.getElementById('counter'),b = parseInt(a.innerText)
+        if(b>0){
+            a.innerText = b-1
+        }
+        else if (b<1){
+            nextJoke();
+        }
+    }, 1000)
+}
+
+pauseButton.addEventListener('click', function(){
+    playing?(playing=false,clearInterval(interval),this.innerText="resume"):(playing=true,interval=timer(),this.innerText='hold')
+})
+function nextJoke(){
+    saveOldJokes()
+    oldJokeArray.push(oldJoke);
+    card.remove();
+    getJokeFromAPI();
+    resetTimer();
+}
+next.addEventListener('click', function(){
+    nextJoke();
+})
+previous.addEventListener('click', function(){
+    card.textContent = oldJoke
+})
+
+
+function renderJoke(jokeData){
     if(jokeData.hasOwnProperty('joke')){
         card.textContent = `${jokeData.joke}`
         jokes.appendChild(card)
@@ -53,24 +77,17 @@ function renderJoke(jokeData){
         jokes.appendChild(card);
     }
 }
-function deleteCurrentJoke(){
-    let card = document.querySelector('.card');
-    let oldJoke = document.createElement('h2');
-    oldJoke.id = 'oldJoke';
-    oldJoke.textContent = card.textContent;
-    card.remove();
-    console.log(oldJoke)
+
+function saveOldJokes(){
+    oldJoke = card.textContent;
+    console.log(oldJoke);
 }
-function getOldJoke(){
-    let card = document.querySelector('.card');
-    console.log(card)
-    let oldJoke = document.getElementById('oldJoke')
-    oldJoke.textContent = card.textContent
-    card.appendChild(oldJoke)
-}
+
 function getJokeFromAPI(){
     fetch('https://v2.jokeapi.dev/joke/any')
     .then(res => res.json())
     .then(jokeData => renderJoke(jokeData))
 }
+nextJoke()
 getJokeFromAPI()
+console.log(oldJoke)
